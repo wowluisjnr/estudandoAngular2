9f76d6dc2c226c7978d60d2fa77fbc558ddde26b
pc mara <wowluisjnr@gmail.com>
2018-07-10 10:44:05 -0300

    Adicionando componente com o comando  g c lista-pessoa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPessoaComponent
   ],
   imports: [
     BrowserModule
