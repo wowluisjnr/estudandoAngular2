9900cd86966d34f84c4fe0fed8d63d9966c2241c
Claudio <wowluisjnr@gmail.com>
2018-07-12 18:22:04 -0300

    ngFor com trackBay para carregar somente elementos que não existam na lista

--- src/app/ng-for/ng-for.component.ts
+++ src/app/ng-for/ng-for.component.ts
@@ -7,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgForComponent implements OnInit {
 
-  nomes: string[]=['João','Maria','Thiago','José'];
+  nomes =[
+    {id:1, nome:'João'},
+    {id:2, nome:'Maria'},
+    {id:3, nome:'Thiago'},
+    {id:4, nome:'José'}];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  meuSave(index:number, nomes:any){
+    return nomes.id;
+  }
+
+  atualizar(){
+    this.nomes = [
+      {id:1, nome:'João'},
+      {id:2, nome:'Maria'},
+      {id:3, nome:'Thiago'},
+      {id:4, nome:'José'},
+      {id:5, nome:'tatat'}
+    ];
   }
 
 }
