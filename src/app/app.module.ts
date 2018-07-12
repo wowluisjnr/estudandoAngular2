28ef0637c05f8839b21ab1c3702421556a66aa5c
Claudio <wowluisjnr@gmail.com>
2018-07-12 16:19:03 -0300

    Inserindo property binding. [property]=

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaPessoaComponent,
-    InterpolationBindingComponent
+    InterpolationBindingComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule
