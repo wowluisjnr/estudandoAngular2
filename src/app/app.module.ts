036baacd8eea995b8201ee5d20e60dd04f963259
pc mara <wowluisjnr@gmail.com>
2018-07-13 08:44:55 -0300

    Revert "Adicionando conceitos de INTERPOLATION, e iniciando Property binding"
    This reverts commit eb0aa0d5fa1fa238a777f732f8ac11da130531cf.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
-import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
-import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaPessoaComponent,
-    InterpolationBindingComponent,
-    PropertyBindingComponent
+    ListaPessoaComponent
   ],
   imports: [
     BrowserModule
