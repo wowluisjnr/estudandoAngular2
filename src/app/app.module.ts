7ceca4c9b1955184a29502cd6c207cda7f3d0c65
pc mara <wowluisjnr@gmail.com>
2018-07-13 11:09:04 -0300

    Introdução a ngContent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
     NgSwitchCaseComponent,
     NgForComponent,
     NgClassComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    NgContentComponent
   ],
   imports: [
     BrowserModule
